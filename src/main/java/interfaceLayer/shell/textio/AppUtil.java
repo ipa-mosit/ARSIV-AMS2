9ce4402700b1ff821c7caa3140f8ebf0053bc4f1
caga <cagataycakir@gmail.com>
2019-05-30 18:07:05 +0300

    package names corrected, pom.xml fixed

--- src/main/java/interfaceLayer/shell/textio/AppUtil.java
+++ src/main/java/interfaceLayer/shell/textio/AppUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package arayuzler.shell.textio;
+package interfaceLayer.shell.textio;
 
 import com.google.gson.Gson;
 import org.beryx.textio.TextTerminal;
